eb57ec0858ef91a07e79deeaf0a884ccccaea2bd
karlaavegga <karlaavegga@gmail.com>
2021-10-19 09:45:01 -0600

    Agregamos la Plataforma

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
@@ -16,7 +17,8 @@ function setup(){
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,20)
+    ground = new Ground(600,height,1200,20);
+    Plataform = new Ground(150,305,300,170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -59,4 +61,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    Plataform.display();
 }
